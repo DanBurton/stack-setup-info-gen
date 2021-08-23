ef9ff59da46e9b4d3cba86d8352bf26f7fcf5cc1
Dan Burton <danburton.email@gmail.com>
2021-08-23 13:43:36 -0400

    add test for 9.2.1-rc1

--- test/Main.hs
+++ test/Main.hs
@@ -121,3 +121,10 @@ main = hspec $ do
         expectedOutput <- readFileUtf8 "output/stack-ghc-9.2.1-alpha2.yaml"
         actualOutput `shouldNotBe` ""
         actualOutput `shouldBe` expectedOutput
+    context "with argument: ghc-9.2.1-rc1" $ do
+      it "produces the correct output" $ do
+        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.2.1-rc1"]
+        let actualOutput = pack actualOutputStr
+        expectedOutput <- readFileUtf8 "output/stack-ghc-9.2.1-rc1.yaml"
+        actualOutput `shouldNotBe` ""
+        actualOutput `shouldBe` expectedOutput
