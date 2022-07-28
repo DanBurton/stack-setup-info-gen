1905f2ff267980dd401b8804b0ad042f9eaa22e7
Dan Burton <danburton.email@gmail.com>
2022-07-28 11:01:47 -0700

    reorg test & add test for 9.4.1-rc1

--- test/Main.hs
+++ test/Main.hs
@@ -7,6 +7,27 @@ import System.IO.Silently
 
 import Stack.Setup.Info.Gen
 
+
+data Strength = Weak | Strong
+
+mkTest' :: Strength -> String -> Spec
+mkTest' strength s = context ("with argument: " <> s) $ do
+  it "produces the correct output" $ do
+    actualOutputStr <- capture_ $ mainWithArgs [fromString s]
+    let actualOutput = pack actualOutputStr
+    actualOutput `shouldNotBe` ""
+    case strength of
+      Strong -> do
+        expectedOutput <- readFileUtf8 $ "output/stack-" <> s <> ".yaml"
+        actualOutput `shouldBe` expectedOutput
+      Weak -> pure ()
+
+mkTestWeak :: String -> Spec
+mkTestWeak = mkTest' Weak
+
+mkTest :: String -> Spec
+mkTest = mkTest' Strong
+
 main :: IO ()
 main = hspec $ do
   describe "stripSurroundings" $ do
@@ -16,115 +37,28 @@ main = hspec $ do
           gv = "8.6.2"
       stripSurroundings gdv gv url `shouldBe` "aarch64-deb8-linux"
 
-  -- NOTE
-  -- Some tests below are commented out, because they fail now.
-  -- Because we used to allow deb8 but now we don't.
-  -- shouldSkipFile and systemNameMapping are so bad and hacky
-
   describe "mainWithArgs" $ do
-    context "with argument: ghc-8.6.2" $ do
-      it "produces the correct output" $ do
-        -- TODO: use locally cached files only
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.6.2"]
-        let actualOutput = pack actualOutputStr
-        -- expectedOutput <- readFileUtf8 "output/stack-ghc-8.6.2.yaml"
-        actualOutput `shouldNotBe` ("" :: Text)
-        -- actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.6.1-beta1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.6.1-beta1"]
-        let actualOutput = pack actualOutputStr
-        -- expectedOutput <- readFileUtf8 "output/stack-ghc-8.6.1-beta1.yaml"
-        actualOutput `shouldNotBe` ("" :: Text)
-        -- actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.8.1-alpha1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.8.1-alpha1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.8.1-alpha1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.8.1-rc1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.8.1-rc1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.8.1-rc1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.8.2-rc1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.8.2-rc1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.8.2-rc1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.10.1-alpha1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.10.1-alpha1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.10.1-alpha1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.10.1-alpha2" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.10.1-alpha2"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.10.1-alpha2.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.10.1-rc1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.10.1-rc1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.10.1-rc1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-8.10.1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-8.10.1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-8.10.1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.0.1-alpha1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.0.1-alpha1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.0.1-alpha1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.0.1-rc1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.0.1-rc1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.0.1-rc1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.0.1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.0.1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.0.1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.2.1-alpha1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.2.1-alpha1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.2.1-alpha1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.2.1-alpha2" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.2.1-alpha2"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.2.1-alpha2.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
-    context "with argument: ghc-9.2.1-rc1" $ do
-      it "produces the correct output" $ do
-        actualOutputStr <- capture_ $ mainWithArgs ["ghc-9.2.1-rc1"]
-        let actualOutput = pack actualOutputStr
-        expectedOutput <- readFileUtf8 "output/stack-ghc-9.2.1-rc1.yaml"
-        actualOutput `shouldNotBe` ""
-        actualOutput `shouldBe` expectedOutput
+    -- NOTE
+    -- Some tests below are weakened, because they fail now.
+    -- Because we used to allow deb8 but now we don't.
+    -- shouldSkipFile and systemNameMapping are so bad and hacky
+    mapM_ mkTestWeak
+      [ "ghc-8.6.2"
+      , "ghc-8.6.1-beta1"
+      ]
+    mapM_ mkTest
+      [ "ghc-8.8.1-alpha1"
+      , "ghc-8.8.1-rc1"
+      , "ghc-8.8.2-rc1"
+      , "ghc-8.10.1-alpha1"
+      , "ghc-8.10.1-alpha2"
+      , "ghc-8.10.1-rc1"
+      , "ghc-8.10.1"
+      , "ghc-9.0.1-alpha1"
+      , "ghc-9.0.1-rc1"
+      , "ghc-9.0.1"
+      , "ghc-9.2.1-alpha1"
+      , "ghc-9.2.1-alpha2"
+      , "ghc-9.2.1-rc1"
+      , "ghc-9.4.1-rc1"
+      ]
